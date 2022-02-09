e831cb6cb9a5a29ee546c6304e4a2138fb52ad64
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2022-02-09 15:37:23 +0100

    Add esbuild-runner

--- src/main.js
+++ src/main.js
@@ -7,36 +7,44 @@ import { usersRouter } from "./routes/usersRoutes.js";
 import Router from "@koa/router";
 import { setupDatabase } from "./database/setup.js";
 import { authenticated } from "./middlewares/authenticated.js";
+import process from "node:process";
 
-await setupDatabase();
-
-const app = new Koa();
-
-app.use(errorHandler());
-app.use(bodyparser());
-
-const router = new Router();
-
-router.use(
-  "/recipes",
-  authenticated,
-  recipesRouter.routes(),
-  recipesRouter.allowedMethods()
-);
-router.use(
-  "/ingredients",
-  ingredientsRouter.routes(),
-  ingredientsRouter.allowedMethods()
-);
-router.use("/users", usersRouter.routes(), usersRouter.allowedMethods());
-
-router.get("/", async (ctx) => {
-  ctx.body = { hello: "world" };
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
 });
 
-app.use(router.routes());
-app.use(router.allowedMethods());
+async function main() {
+  await setupDatabase();
 
-app.listen(3000, () => {
-  console.log("Server is up on http://localhost:3000");
-});
+  const app = new Koa();
+
+  app.use(errorHandler());
+  app.use(bodyparser());
+
+  const router = new Router();
+
+  router.use(
+    "/recipes",
+    authenticated,
+    recipesRouter.routes(),
+    recipesRouter.allowedMethods()
+  );
+  router.use(
+    "/ingredients",
+    ingredientsRouter.routes(),
+    ingredientsRouter.allowedMethods()
+  );
+  router.use("/users", usersRouter.routes(), usersRouter.allowedMethods());
+
+  router.get("/", async (ctx) => {
+    ctx.body = { hello: "world" };
+  });
+
+  app.use(router.routes());
+  app.use(router.allowedMethods());
+
+  app.listen(3000, () => {
+    console.log("Server is up on http://localhost:3000");
+  });
+}
